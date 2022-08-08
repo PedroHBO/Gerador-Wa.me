59c5cd92d255eea74087c33e05b6121bc239cc65
Pedro Henrique Batista de Oliveira <pedro.oliveira@CONSTRUTORA.LOCAL>
2022-08-08 09:00:56 -0300

    alteração mensagem

--- telefone.js
+++ telefone.js
@@ -13,13 +13,13 @@ const tel = newArr.join("");
 const user = 'Pedro Henrique';
  now = new Date
 if (now.getHours() >= 7 && now.getHours() < 12) {
-    console.log('//wa.me/+55' + tel + `?text=Bom%20dia,%20meu%20nome%20é%20${user},%20sou%20do%20setor%20de%20TI,%20recebi%20sua%20solicitação%20no%20fluig`);
+    console.log('//wa.me/+55' + tel + `?text=Bom%20dia,%20meu%20nome%20é%20${user},%20sou%20do%20setor%20de%20TI,%20recebi%20sua%20solicitação`);
 }
 else if (now.getHours() >= 12 && now.getHours() < 18) {
-    console.log('//wa.me/+55' + tel + `?text=Boa%20tarde,%20meu%20nome%20é%20${user},%20sou%20do%20setor%20de%20TI,%20recebi%20sua%20solicitação%20no%20fluig`);
+    console.log('//wa.me/+55' + tel + `?text=Boa%20tarde,%20meu%20nome%20é%20${user},%20sou%20do%20setor%20de%20TI,%20recebi%20sua%20solicitação`);
 }
 else{
-    console.log('//wa.me/+55' + tel + `?text=Boa%20noite,%20meu%20nome%20é%20${user},%20sou%20do%20setor%20de%20TI,%20recebi%20sua%20solicitação%20no%20fluig`);
+    console.log('//wa.me/+55' + tel + `?text=Boa%20noite,%20meu%20nome%20é%20${user},%20sou%20do%20setor%20de%20TI,%20recebi%20sua%20solicitação`);
 }
 
 
